4db3c22768551daf7a7200895eab8ad51e477665
01likhitha <116881659+01likhitha@users.noreply.github.com>
2022-12-01 11:00:38 +0530

    redirect issues

--- myproject1/myproject1/bankingapp/views.py
+++ myproject1/myproject1/bankingapp/views.py
@@ -95,8 +95,8 @@ def newUser(request):
         if continuev and emptyvalues:
             usr = models.Registration(username=username, password=password)
             usr.save()
-            return redirect('/')
-    return render(request, 'register.html',{'user': user})
+           
+    return render(request, 'login.html',{'user': user})
 
 def login1(request):
     if 'username' in request.POST:
