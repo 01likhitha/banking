8bf723f675068acaf90d8cacb70f90f320252a9f
01likhitha <116881659+01likhitha@users.noreply.github.com>
2022-12-01 11:41:56 +0530

    redirect

--- myproject1/myproject1/bankingapp/views.py
+++ myproject1/myproject1/bankingapp/views.py
@@ -84,6 +84,7 @@ def newUser(request):
         password = request.POST['password']
         cpassword = request.POST['cpassword']
         emptyvalues=True
+        return redirect('login.html')
 
         if username=='' or password=='' or cpassword=='':
             emptyvalues=False
